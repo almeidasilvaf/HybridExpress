26797a6b9776107292733e24fefb8fe0831fce72
almeidasilvaf <fabricio_almeidasilva@hotmail.com>
2023-10-21 20:09:12 +0200

    Added example data sets with documentation

--- R/data.R
+++ R/data.R
@@ -12,3 +12,35 @@
 #' data(se_chlamy)
 #' @usage data(se_chlamy)
 "se_chlamy"
+
+
+#' List of differentially expressed genes for all contrasts
+#'
+#' This object was obtained with \code{get_deg_list()} using the example
+#' data set \strong{se_chlamy}.
+#' 
+#' @name deg_list
+#' @format A list of data frames with gene-wise test statistics for
+#' differentially expressed genes for each contrast. Contrasts are
+#' "P1_vs_P2", "P1_vs_F1", "P2_vs_F1", and "midparent_vs_F1".
+#' @examples 
+#' data(deg_list)
+#' @usage data(deg_list)
+"deg_list"
+
+
+#' Data frame with frequencies (absolute and relative) of DEGs per contrast
+#'
+#' This object was obtained with \code{get_deg_counts()} using the example
+#' data set \strong{deg_list}.
+#' 
+#' @name deg_counts
+#' @format A data frame with the frequencies (absolute and relative) of
+#' up- and down-regulated genes in each contrast. Relative frequencies
+#' are calculated relative to the total number of genes in the count matrix
+#' used for differential expression analysis.
+#' @examples 
+#' data(deg_counts)
+#' @usage data(deg_counts)
+"deg_counts"
+
